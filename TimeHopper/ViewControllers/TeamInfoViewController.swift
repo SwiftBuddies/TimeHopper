1bbb3c95220dcc2446de2e1488b0731a30267a6d
Elena Sharipova <elena.sh.r@yandex.ru>
2023-04-26 16:29:36 +0300

    feat: team info view in progress

---
+++ TimeHopper/ViewControllers/TeamInfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TeamInfoViewController.swift
+//  TimeHopper
+//
+//  Created by Elena Sharipova on 26.04.2023.
+//
+
+import UIKit
+
+class TeamInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
