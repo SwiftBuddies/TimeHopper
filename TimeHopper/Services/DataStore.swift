bf95265b8861caa31f8573d14b4907c49e0b4c9f
Julia Kazarina <120021628+kazarinaJu@users.noreply.github.com>
2023-04-27 19:00:41 +0500

    added data for getRoute

--- TimeHopper/Services/DataStore.swift
+++ TimeHopper/Services/DataStore.swift
@@ -5,4 +5,20 @@
 //  Created by SwiftBuddies on 25.04.2023.
 //
 
-
+final class DataStore {
+    
+    static let shared = DataStore()
+    
+    let universes = [" ", " ", " "]
+    let locations =  [" ", " ", "тут тайный маршрут"]
+    let years = [1,2,3,4]
+    let routeNames = [" ", " "]
+    let routeDescriptions = [" ", " "]
+    
+    
+    
+    // тут не всё
+    
+    
+    private init() {}
+}
