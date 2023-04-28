8e945e90128629fdf134016f8714b392baf7e94a
Elena Sharipova <elena.sh.r@yandex.ru>
2023-04-28 14:40:41 +0300

    feat: cornerRadius for team photos implemented

--- TimeHopper/ViewControllers/TeamInfoViewController.swift
+++ TimeHopper/ViewControllers/TeamInfoViewController.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class TeamInfoViewController: UIViewController {
-
-
+    
+    @IBOutlet var juliaKazarinaPhoto: UIImageView!
+    @IBOutlet var elenaDemchenkoPhoto: UIImageView!
+    @IBOutlet var pavelKarunnyjPhoto: UIImageView!
+    @IBOutlet var elenaSharipovaPhoto: UIImageView!
+    
+    override func viewWillLayoutSubviews() {
+        juliaKazarinaPhoto.layer.cornerRadius = juliaKazarinaPhoto.frame.width / 2
+        elenaDemchenkoPhoto.layer.cornerRadius = elenaDemchenkoPhoto.frame.width / 2
+        pavelKarunnyjPhoto.layer.cornerRadius = pavelKarunnyjPhoto.frame.width / 2
+    }
+    
 }
