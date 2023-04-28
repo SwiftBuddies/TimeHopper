57d9c9a05498da62864e0da8d0b4272d268b64c6
Elena Sharipova <elena.sh.r@yandex.ru>
2023-04-28 14:02:44 +0300

    feat: team info view implemented

--- TimeHopper/ViewControllers/TeamInfoViewController.swift
+++ TimeHopper/ViewControllers/TeamInfoViewController.swift
@@ -2,15 +2,12 @@
 //  TeamInfoViewController.swift
 //  TimeHopper
 //
-//  Created by Elena Sharipova on 26.04.2023.
+//  Created by Elena Sharipova on 28.04.2023.
 //
 
 import UIKit
 
 class TeamInfoViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
 
 }
