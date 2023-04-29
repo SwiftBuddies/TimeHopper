d7eb46dc7aa25e46ec33443315198b961385c128
Julia Kazarina <120021628+kazarinaJu@users.noreply.github.com>
2023-04-29 11:12:51 +0500

    set button title

--- TimeHopper/ViewControllers/QuestionsViewController.swift
+++ TimeHopper/ViewControllers/QuestionsViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 final class QuestionsViewController: UIViewController {
-
+    
     @IBOutlet var questionTopicLabel: UILabel!
     
     @IBOutlet var firstAnswer: UIButton!
     @IBOutlet var secondAnswer: UIButton!
     @IBOutlet var thirdAnswer: UIButton!
+    
+    let universes = Universe.getUniverses(from: DataStore.shared.universes)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,14 +23,15 @@ final class QuestionsViewController: UIViewController {
         firstAnswer.layer.cornerRadius = 10
         secondAnswer.layer.cornerRadius = 10
         thirdAnswer.layer.cornerRadius = 10
-       
+        
         navigationItem.backBarButtonItem = UIBarButtonItem(
             title: "Ой, нет",
             style: .plain,
             target: nil,
             action: nil
         )
+        
+        firstAnswer.setTitle(universes[0].title, for: .normal)
+        secondAnswer.setTitle(universes[1].title, for: .normal)
     }
-    
-
 }
