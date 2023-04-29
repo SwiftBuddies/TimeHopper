5ccfc5502a3984752b826d8b85739cb7b67ee56b
paveld3zu <pavel.karunnyj@yandex.ru>
2023-04-29 12:44:37 +0300

    Add alert, setup keyboard

--- TimeHopper/ViewControllers/WelcomeViewController.swift
+++ TimeHopper/ViewControllers/WelcomeViewController.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
-
+final class WelcomeViewController: UIViewController {
+    
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var timeMachineButton: UIButton!
     
@@ -18,35 +18,53 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         timeMachineButton.layer.cornerRadius = 10
-        
+        nameTextField.delegate = self
         print(universes)
         
         // это пример использования на последующих экранах, куда будет передаваться universes
         // при клике на кнопку в массиве будет поиск элемента с тайтлом, равным тексту кнопки
         let universe = universes.first(where: {$0.title == nashMir})
-        print(universe)
+        print(universe ?? "")
     }
-//
-//
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        super .touchesBegan(touches, with: event)
-//        view.endEditing(true)
-//    }
-//
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        if segue.identifier == "showQuestionsViewController" {
-//            let questionsVC = segue.destination as? QuestionsViewController
-//            questionsVC.userName = nameTextField.text
-//        } else {
-//            guard let teamInfoVC = segue.destination as? TeamInfoViewController else { return }
-//            teamInfoVC.memberTeam = memberTeam
-//        }
-//    }
-//
-//    @IBAction func timeMachineButton(_ sender: UIButton) {
-//        performSegue(withIdentifier: "showQuestionsViewController", sender: nil)
-//    }
-//    @IBAction func aboutTeamButton(_ sender: UIButton) {
-//    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let questionsVC = segue.destination as? QuestionsViewController {
+            //            questionsVC.userName = nameTextField.text
+            //            questionsVC.universes = universes
+        } else {
+            guard let teamInfoVC = segue.destination as? TeamInfoViewController else { return }
+            //            teamInfoVC.memberTeam = memberTeam
+        }
+    }
+    
+    @IBAction func timeMachineButton(_ sender: UIButton) {
+        if nameTextField.text == "" {
+            showAlert(title: "Оуууу", message: "Дружище, как тебя зовут?")
+        }
+        performSegue(withIdentifier: "showQuestionsVC", sender: nil)
+    }
+    
+    @IBAction func aboutTeamButton(_ sender: UIButton) {
+    }
+    
+    private func  showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+        }
 }
 
+extension WelcomeViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+}
