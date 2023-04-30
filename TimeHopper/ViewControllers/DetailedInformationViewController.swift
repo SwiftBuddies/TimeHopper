622a6a7c0ff60fd75647c1f01fb077d57b311a6c
paveld3zu <pavel.karunnyj@yandex.ru>
2023-04-30 22:59:51 +0300

    Setup segue

--- TimeHopper/ViewControllers/DetailedInformationViewController.swift
+++ TimeHopper/ViewControllers/DetailedInformationViewController.swift
@@ -15,7 +15,7 @@ final class DetailedInformationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.hidesBackButton = true
+//        navigationItem.hidesBackButton = true
         navigationItem.title = route.title
         detailedInformation.text = route.description
     }
